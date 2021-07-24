9fc38f8a4d35fd6d87d2d99ac1a4dff465b15eed
Aleksandar Cvjetean <31933549+crypt096@users.noreply.github.com>
2021-07-24 17:34:17 +0200

    Use graphql inside React component using useQuery hook and gql query.

--- frontend/src/pages/Homepage.js
+++ frontend/src/pages/Homepage.js
@@ -1,17 +1,27 @@
 import React from 'react'
-import useFetch from '../hooks/useFetch';
 import { Link } from 'react-router-dom';
+import { useQuery, gql } from '@apollo/client';
+
+const REVIEWS = gql`
+    query getReviews {
+        reviews {
+            id
+            title
+            rating
+            body
+        }
+    }
+`
 
 export default function Homepage() {
-    const { loading, error, data } = useFetch('http://localhost:1337/reviews');
+    const {loading, error, data} = useQuery(REVIEWS);
 
-    if (loading) return <p>Loading...</p>
-
-    if (error) return <p>Error...</p>
+    if(loading) return <p>Loading...</p>
+    if(error) return <p>Error :(</p>
 
     return (
         <div>
-            {data.map(review => (
+            {data.reviews.map(review => (
                 <div key={review.id} className="review-card">
                     <div className="rating">{review.rating}</div>
                     <h2>{review.title}</h2>
